322e1d1c57e3c9f6f7f5615428933d7249dff912
olegato <oleg.egunkov@yajndex.ru>
2020-07-28 16:29:20 +0300

    add state managment structure

--- src/stores/startupSaga.js
+++ src/stores/startupSaga.js
@@ -1,8 +1,7 @@
 import { call } from 'redux-saga/effects'
-import  { testRequest } from '../services/api'
 import axios from 'axios'
 
 export default function* helloSaga() {
   const response = yield call(axios.get, 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=YFqGlgVMINF5QD1AvpdyGXeaMm1nsI8r1BsSBVF4')
-  console.log(response.data)
+  console.log(response)
 }
