e6e13eefce4f9cebc29b11399498dd0ec2a0a39c
olegato <oleg.egunkov@yajndex.ru>
2020-08-04 19:58:54 +0300

    add base store

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,18 +1,20 @@
 import { combineReducers, createStore,  applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
 // import { composeWithDevTools } from 'redux-devtools-extension'
 
-import testStore from './testStore/testRedux'
+import marsGalleryStore from 'stores/marsGalleryStore/marsGalleryRedux'
 import startupSaga from './startupSaga'
+import { fetchMarsPhotos} from 'stores/marsGalleryStore/Sagas/fetchMarsPhotos'
 
 const reducers = combineReducers({
-  testStore
+  marsGalleryStore
 })
 
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware)))
 
-sagaMiddleware.run(startupSaga)
+sagaMiddleware.run(fetchMarsPhotos)
 
 export default store
---
+++ src/stores/marsGalleryStore/Sagas/index.js
@@ -0,0 +1,10 @@
+// import { takeLatest, all } from 'redux-saga/effects'
+// import
+
+// function * sagas () {
+//   yield all([
+//     takeLatest(WidgetsAsTypes.WIDGETS_AS_REQUEST, fetchAsWidget)
+//   ])
+// }
+
+// export default sagas
